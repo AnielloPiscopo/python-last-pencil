d4b31a3a160a7d38384eba582a74339f2176ea3c
Aniello Piscopo <95495488+AnielloPiscopo@users.noreply.github.com>
2025-12-08 10:13:02 +0100

    Update custom_exceptions.py by adding general docstrings

--- custom_exceptions.py
+++ custom_exceptions.py
@@ -1,5 +1,7 @@
 class ConversionError(ValueError):
+    """Raised when a value cannot be converted."""
     pass
 
 class ConstraintError(ValueError):
+    """Raised when a value violates logical constraints."""
     pass
